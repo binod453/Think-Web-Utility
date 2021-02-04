ab437a31cf99aa189e897a8d331ea8f3d7894177
binod <kumar.binod453@gmail.com>
2021-02-04 10:10:33 +0530

    code base update with static data

--- ThinkUtility/src/main/java/com/mps/think/utility/configuration/WebSecurityConfig.java
+++ ThinkUtility/src/main/java/com/mps/think/utility/configuration/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.mps.think.utility.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,10 +25,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()))
                 .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
-//                .and()
-//                .formLogin()
-//					.loginPage("/login")
-//					.permitAll();
     } 
+	
+	@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("password").roles("ADMIN");
+    }
 	
 }
--- ThinkUtility/src/main/java/com/mps/think/utility/controller/LoginController.java
+++ ThinkUtility/src/main/java/com/mps/think/utility/controller/LoginController.java
@@ -1,27 +1,42 @@
 package com.mps.think.utility.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+import com.mps.think.utility.dao.LoginDao;
+
+@Controller
 public class LoginController {
 
-    @RequestMapping(value={"/"}, method = RequestMethod.GET)
-    public ModelAndView login(){
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("login");
-        return modelAndView;
-    }
+	@Autowired
+	LoginDao loginDao;
 
-	
-	@RequestMapping(path = "/login", method = RequestMethod.POST)
+	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
+	public String login() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login");
+		return "login";
+	}
+
+	@RequestMapping(path = "/dynamicRenewalCodes", method = RequestMethod.POST)
 	public String login(@RequestParam(value = "username") String username,
 			@RequestParam(value = "password") String password) {
-		return null;
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("dynamicRenewalCodeList");
+		return "dynamicRenewalCodeList";
+	}
+
+	@PostMapping(value = "/addNewRenewalCode")
+	public String addNewRenewalCode(Model model) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("addRenewalCode");
+		return "addRenewalCode";
 	}
 
 }
