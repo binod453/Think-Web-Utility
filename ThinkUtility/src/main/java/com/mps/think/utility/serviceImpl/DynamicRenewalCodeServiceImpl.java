b97af87ea4cded3664412662387d0a3df32909e3
binod453 <kumar.binod453@gmail.com>
2021-03-30 09:48:22 +0530

    Code cleanup and add logger

--- ThinkUtility/src/main/java/com/mps/think/utility/serviceImpl/DynamicRenewalCodeServiceImpl.java
+++ ThinkUtility/src/main/java/com/mps/think/utility/serviceImpl/DynamicRenewalCodeServiceImpl.java
@@ -3,7 +3,6 @@ package com.mps.think.utility.serviceImpl;
 import java.math.BigDecimal;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
@@ -12,6 +11,8 @@ import java.util.stream.Stream;
 
 import javax.xml.rpc.ServiceException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.google.gson.JsonObject;
@@ -22,35 +23,27 @@ import com.mps.think.utility.service.DynamicRenewalCodeService;
 
 import Think.XmlWebServices.Dynamic_price;
 import Think.XmlWebServices.Dynamic_price_add_request;
-import Think.XmlWebServices.Dynamic_price_add_response;
 import Think.XmlWebServices.Dynamic_price_card;
 import Think.XmlWebServices.Dynamic_price_card_add_request;
-import Think.XmlWebServices.Dynamic_price_card_add_response;
 import Think.XmlWebServices.Dynamic_price_card_edit_request;
-import Think.XmlWebServices.Dynamic_price_card_edit_response;
 import Think.XmlWebServices.Dynamic_price_card_select_request;
-import Think.XmlWebServices.Dynamic_price_card_select_response;
 import Think.XmlWebServices.Dynamic_price_card_select_responseDynamic_price_card_select;
 import Think.XmlWebServices.Dynamic_price_edit_request;
-import Think.XmlWebServices.Dynamic_price_edit_response;
 import Think.XmlWebServices.Dynamic_price_select_request;
-import Think.XmlWebServices.Dynamic_price_select_response;
-import Think.XmlWebServices.Dynamic_price_select_responseDynamic_price_id;
 import Think.XmlWebServices.Dynamic_price_select_responseDynamic_price_select;
-import Think.XmlWebServices.Payment_type_select_request;
-import Think.XmlWebServices.Payment_type_select_responsePayment_type;
 import Think.XmlWebServices.ThinkSoap;
 import Think.XmlWebServices.ThinkWSLocator;
 import Think.XmlWebServices.User_login_data;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service
 public class DynamicRenewalCodeServiceImpl implements DynamicRenewalCodeService {
+	
+	Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Override
 	public List<Dynamic_price_select_responseDynamic_price_select> getDynamicCodesList(LoginModel loginModel)
 			throws ServiceException, RemoteException {
+		log.info("Inside method : getDynamicCodesList");
 
 		List<Dynamic_price_select_responseDynamic_price_select> dynamicPriceSelectResponse = new ArrayList<Dynamic_price_select_responseDynamic_price_select>();
 
@@ -71,71 +64,69 @@ public class DynamicRenewalCodeServiceImpl implements DynamicRenewalCodeService 
 				.sorted(Comparator.comparingInt(Dynamic_price_select_responseDynamic_price_select::getDynamic_price_id))
 				.collect(Collectors.toList());
 
+		log.info("Complete method : getDynamicCodesList");
 		return dynamicPriceSelectResponse;
 	}
 
 	@Override
 	public int addNewDynamicRenewalCode(DynamicRenewalCodeModel dynamicRenewalCodeModel)
 			throws ServiceException, RemoteException {
+		log.info("Inside method : addNewDynamicRenewalCode");
 		ThinkSoap soap = null;
 		ThinkWSLocator locator = new ThinkWSLocator();
 		soap = locator.getThinkSoap();
-		
-		int seq=0;
-		
-		//Check whether the data is duplicate or not
-//		int status = checkDuplicateData(dynamicRenewalCodeModel);
-//		if(status == 1) {
-//			return status;
-//		}else {
-			User_login_data loginData = new User_login_data();
-			Dynamic_price dynamicPrice = new Dynamic_price();
 
-			List<RenewalCodeDataDetails> renewalPriceCardsList = dynamicRenewalCodeModel.getRenewalCodeDataDetailsList();
-			
-//			List<RenewalCodeDataDetails> filteredRenewalPriceCardsList = renewalPriceCardsList.stream().filter(data -> data.getFromCycle() > 0).collect(Collectors.toList());
-			renewalPriceCardsList.removeIf(data -> data.getFromCycle() == 0);
-			
+		int seq = 0;
 
-			Dynamic_price_add_request priceAddRequest = setDynamicPriceAddRequest(dynamicRenewalCodeModel, loginData,
-					dynamicPrice);
-			soap.dynamicPriceAdd(priceAddRequest);
+		User_login_data loginData = new User_login_data();
+		Dynamic_price dynamicPrice = new Dynamic_price();
 
-			for (RenewalCodeDataDetails priceCardDetail : renewalPriceCardsList) {
-				priceCardDetail.setDynamicPriceSeq(seq++);
-				Dynamic_price_card dynamicPriceCard = new Dynamic_price_card();
-				Dynamic_price_card_add_request dynamicPriceCardAddRequest = setDynamicPriceCardAddRequest(
-						dynamicRenewalCodeModel, priceCardDetail, loginData, dynamicPriceCard);
-				soap.dynamicPricecardAdd(dynamicPriceCardAddRequest);
-			}
+		List<RenewalCodeDataDetails> renewalPriceCardsList = dynamicRenewalCodeModel.getRenewalCodeDataDetailsList();
 
-			return 0;
-		//}
-		
-		
+		renewalPriceCardsList.removeIf(data -> data.getFromCycle() == 0);
+
+		Dynamic_price_add_request priceAddRequest = setDynamicPriceAddRequest(dynamicRenewalCodeModel, loginData,
+				dynamicPrice);
+		soap.dynamicPriceAdd(priceAddRequest);
+
+		for (RenewalCodeDataDetails priceCardDetail : renewalPriceCardsList) {
+			priceCardDetail.setDynamicPriceSeq(seq++);
+			Dynamic_price_card dynamicPriceCard = new Dynamic_price_card();
+			Dynamic_price_card_add_request dynamicPriceCardAddRequest = setDynamicPriceCardAddRequest(
+					dynamicRenewalCodeModel, priceCardDetail, loginData, dynamicPriceCard);
+			soap.dynamicPricecardAdd(dynamicPriceCardAddRequest);
+		}
+
+		log.info("Complete method : addNewDynamicRenewalCode");
+		return 0;
 	}
 
 	@Override
 	public LoginModel setLoginDetails(JsonObject jsonObj) {
+		log.info("Inside method : setLoginDetails");
 		LoginModel loginModel = new LoginModel();
 		loginModel.setUsername(jsonObj.get("UserName").getAsString());
 		loginModel.setUserCode(jsonObj.get("userCode").getAsString());
 		loginModel.setPublisher(jsonObj.get("publisher").getAsString());
 		loginModel.setToken(jsonObj.get("Token").getAsString());
 		loginModel.setStatus(jsonObj.get("Status").getAsString());
+		log.info("Complete method : setLoginDetails");
 		return loginModel;
 	}
 
 	@Override
 	public int getMaxCodeId(LoginModel loginModel) throws ServiceException, RemoteException {
+		log.info("Inside method : getMaxCodeId");
 		List<Dynamic_price_select_responseDynamic_price_select> data = getDynamicCodesList(loginModel);
 		int result = data.get(data.size() - 1).getDynamic_price_id();
+		log.info("Complete method : getMaxCodeId");
 		return result;
 	}
 
 	@Override
 	public DynamicRenewalCodeModel getDynamicCodesById(LoginModel userDetails, int id)
 			throws ServiceException, RemoteException {
+		log.info("Inside method : getDynamicCodesById");
 		ThinkSoap soap = null;
 		ThinkWSLocator locator = new ThinkWSLocator();
 		soap = locator.getThinkSoap();
@@ -177,37 +168,37 @@ public class DynamicRenewalCodeServiceImpl implements DynamicRenewalCodeService 
 				if (!Objects.isNull(select.getPrice()) && select.getPrice().intValueExact() > 0) {
 					codeDataDetails.setValue(
 							Objects.isNull(select.getPrice().intValueExact()) ? 0 : select.getPrice().intValueExact());
-				}
-				else if (!Objects.isNull(select.getPercentage()) && select.getPercentage().intValueExact() > 0) {
+				} else if (!Objects.isNull(select.getPercentage()) && select.getPercentage().intValueExact() > 0) {
 					codeDataDetails.setValue(Objects.isNull(select.getPercentage().intValueExact()) ? 0
 							: select.getPercentage().intValueExact());
-				}
-				else {
+				} else {
 					codeDataDetails.setValue(0);
 				}
-				codeDataDetails.setType(
-						Objects.isNull(select.getDynamic_price_type()) ? 0 : select.getDynamic_price_type());
+				codeDataDetails
+						.setType(Objects.isNull(select.getDynamic_price_type()) ? 0 : select.getDynamic_price_type());
 				codeDataDetails.setCurrency(Objects.isNull(select.getCurrency()) ? "" : select.getCurrency());
 
 				codeDataDetailsList.add(codeDataDetails);
 			}
 			dynamicRenewalCodeModel.setRenewalCodeDataDetailsList(codeDataDetailsList);
 		}
-
+		log.info("Complete method : getDynamicCodesById");
 		return dynamicRenewalCodeModel;
 	}
 
 	@Override
 	public int updateDynamicRenewalCode(DynamicRenewalCodeModel dynamicRenewalCodeModel)
 			throws ServiceException, RemoteException {
+		log.info("Inside method : updateDynamicRenewalCode");
 		ThinkSoap soap = null;
 		ThinkWSLocator locator = new ThinkWSLocator();
 		soap = locator.getThinkSoap();
 
 		User_login_data loginData = new User_login_data();
 		Dynamic_price dynamicPrice = new Dynamic_price();
-		
-		List<RenewalCodeDataDetails> editRenewalPriceCardsList = dynamicRenewalCodeModel.getRenewalCodeDataDetailsList();
+
+		List<RenewalCodeDataDetails> editRenewalPriceCardsList = dynamicRenewalCodeModel
+				.getRenewalCodeDataDetailsList();
 
 		Dynamic_price_edit_request priceEditRequest = setDynamicPriceEditRequest(dynamicRenewalCodeModel, loginData,
 				dynamicPrice);
@@ -219,13 +210,14 @@ public class DynamicRenewalCodeServiceImpl implements DynamicRenewalCodeService 
 					dynamicRenewalCodeModel, priceCardDetail, loginData, dynamicPriceCard);
 			soap.dynamicPricecardEdit(dynamicPriceCardEditRequest);
 		}
-
+		log.info("Complete method : updateDynamicRenewalCode");
 		return 0;
 	}
 
 	private Dynamic_price_card_edit_request setDynamicPriceCardEditRequest(
-			DynamicRenewalCodeModel dynamicRenewalCodeModel, RenewalCodeDataDetails priceCardDetail, User_login_data loginData,
-			Dynamic_price_card dynamicPriceCard) {
+			DynamicRenewalCodeModel dynamicRenewalCodeModel, RenewalCodeDataDetails priceCardDetail,
+			User_login_data loginData, Dynamic_price_card dynamicPriceCard) {
+		log.info("Inside method : setDynamicPriceCardEditRequest");
 		Dynamic_price_card_edit_request priceCardEditRequest = new Dynamic_price_card_edit_request();
 
 		loginData.setLogin(dynamicRenewalCodeModel.getLoginModel().getUsername());
@@ -233,27 +225,19 @@ public class DynamicRenewalCodeServiceImpl implements DynamicRenewalCodeService 
 		priceCardEditRequest.setDsn(dynamicRenewalCodeModel.getLoginModel().getDsn());
 
 		dynamicPriceCard
-				.setFrom_cycle(Objects.isNull(priceCardDetail.getFromCycle()) ? 0
-						: priceCardDetail.getFromCycle());
-		dynamicPriceCard
-				.setTo_cycle(Objects.isNull(priceCardDetail.getToCycle()) ? 0
-						: priceCardDetail.getToCycle());
-		if (!Objects.isNull(priceCardDetail.getType())
-				&& priceCardDetail.getType() == 0) {
-			dynamicPriceCard.setPrice(
-					Objects.isNull(priceCardDetail.getValue()) ? BigDecimal.ZERO
-							: BigDecimal.valueOf(priceCardDetail.getValue()));
+				.setFrom_cycle(Objects.isNull(priceCardDetail.getFromCycle()) ? 0 : priceCardDetail.getFromCycle());
+		dynamicPriceCard.setTo_cycle(Objects.isNull(priceCardDetail.getToCycle()) ? 0 : priceCardDetail.getToCycle());
+		if (!Objects.isNull(priceCardDetail.getType()) && priceCardDetail.getType() == 0) {
+			dynamicPriceCard.setPrice(Objects.isNull(priceCardDetail.getValue()) ? BigDecimal.ZERO
+					: BigDecimal.valueOf(priceCardDetail.getValue()));
 		} else {
-			dynamicPriceCard.setPercentage(
-					Objects.isNull(priceCardDetail.getValue()) ? BigDecimal.ZERO
-							: BigDecimal.valueOf(priceCardDetail.getValue()));
+			dynamicPriceCard.setPercentage(Objects.isNull(priceCardDetail.getValue()) ? BigDecimal.ZERO
+					: BigDecimal.valueOf(priceCardDetail.getValue()));
 		}
-		dynamicPriceCard.setDynamic_price_type(
-				Objects.isNull(priceCardDetail.getType()) ? 0
-						: priceCardDetail.getType());
 		dynamicPriceCard
-				.setCurrency(Objects.isNull(priceCardDetail.getCurrency()) ? ""
-						: priceCardDetail.getCurrency());
+				.setDynamic_price_type(Objects.isNull(priceCardDetail.getType()) ? 0 : priceCardDetail.getType());
+		dynamicPriceCard
+				.setCurrency(Objects.isNull(priceCardDetail.getCurrency()) ? "" : priceCardDetail.getCurrency());
 		dynamicPriceCard.setDynamic_price_id(dynamicRenewalCodeModel.getSlNo());
 		dynamicPriceCard.setDynamic_price_seq(priceCardDetail.getDynamicPriceSeq());
 
@@ -261,11 +245,13 @@ public class DynamicRenewalCodeServiceImpl implements DynamicRenewalCodeService 
 		priceCardEditRequest.setDynamic_price_card(dynamicPriceCard);
 		priceCardEditRequest.setSubmit(true);
 
+		log.info("Complete method : setDynamicPriceCardEditRequest");
 		return priceCardEditRequest;
 	}
 
 	private Dynamic_price_edit_request setDynamicPriceEditRequest(DynamicRenewalCodeModel dynamicRenewalCodeModel,
 			User_login_data loginData, Dynamic_price dynamicPrice) {
+		log.info("Inside method : setDynamicPriceEditRequest");
 		Dynamic_price_edit_request priceEditRequest = new Dynamic_price_edit_request();
 
 		dynamicPrice.setDynamic_price_name(dynamicRenewalCodeModel.getDynamicCode());
@@ -281,12 +267,14 @@ public class DynamicRenewalCodeServiceImpl implements DynamicRenewalCodeService 
 		priceEditRequest.setDynamic_price(dynamicPrice);
 		priceEditRequest.setSubmit(true);
 
+		log.info("Complete method : setDynamicPriceEditRequest");
 		return priceEditRequest;
 	}
 
 	private Dynamic_price_card_add_request setDynamicPriceCardAddRequest(
 			DynamicRenewalCodeModel dynamicRenewalCodeModel, RenewalCodeDataDetails priceCardDetail,
 			User_login_data loginData, Dynamic_price_card dynamicPriceCard) {
+		log.info("Inside method : setDynamicPriceCardAddRequest");
 		Dynamic_price_card_add_request priceCardAddRequest = new Dynamic_price_card_add_request();
 
 		loginData.setLogin(dynamicRenewalCodeModel.getLoginModel().getUsername());
@@ -303,8 +291,8 @@ public class DynamicRenewalCodeServiceImpl implements DynamicRenewalCodeService 
 			dynamicPriceCard.setPercentage(Objects.isNull(priceCardDetail.getValue()) ? BigDecimal.ZERO
 					: BigDecimal.valueOf(priceCardDetail.getValue()));
 		}
-		dynamicPriceCard.setDynamic_price_type(
-				Objects.isNull(priceCardDetail.getType()) ? 0 : priceCardDetail.getType());
+		dynamicPriceCard
+				.setDynamic_price_type(Objects.isNull(priceCardDetail.getType()) ? 0 : priceCardDetail.getType());
 		dynamicPriceCard
 				.setCurrency(Objects.isNull(priceCardDetail.getCurrency()) ? "" : priceCardDetail.getCurrency());
 
@@ -315,11 +303,13 @@ public class DynamicRenewalCodeServiceImpl implements DynamicRenewalCodeService 
 		priceCardAddRequest.setDynamic_price_card(dynamicPriceCard);
 		priceCardAddRequest.setSubmit(true);
 
+		log.info("Complete method : setDynamicPriceCardAddRequest");
 		return priceCardAddRequest;
 	}
 
 	private Dynamic_price_add_request setDynamicPriceAddRequest(DynamicRenewalCodeModel dynamicRenewalCodeModel,
 			User_login_data loginData, Dynamic_price dynamicPrice) {
+		log.info("Inside method : setDynamicPriceAddRequest");
 		Dynamic_price_add_request priceAddRequest = new Dynamic_price_add_request();
 
 		dynamicPrice.setDynamic_price_name(dynamicRenewalCodeModel.getDynamicCode());
@@ -335,22 +325,7 @@ public class DynamicRenewalCodeServiceImpl implements DynamicRenewalCodeService 
 		priceAddRequest.setDynamic_price(dynamicPrice);
 		priceAddRequest.setSubmit(true);
 
+		log.info("Complete method : setDynamicPriceAddRequest");
 		return priceAddRequest;
-	}
-
-	private int checkDuplicateData(DynamicRenewalCodeModel dynamicRenewalCodeModel) throws RemoteException, ServiceException {
-		List<Dynamic_price_select_responseDynamic_price_select> dataList = getDynamicCodesList(dynamicRenewalCodeModel.getLoginModel());
-		Dynamic_price_select_responseDynamic_price_select unique = dataList.stream()
-																			.filter(data -> data.getDynamic_price_name().equals(dynamicRenewalCodeModel.getDynamicCode()))
-																			.findAny()
-																			.orElse(null);
-		
-		if(unique != null) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
-		
 	}
 }
